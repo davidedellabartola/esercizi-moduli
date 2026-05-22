be9ef63b3cda8f95c471ce94234632502c79d80b
davidedellabartola <davide.dellabartola2005@gmail.com>
2026-05-22 10:27:20 +0200

    creata cartella conversioni e modulo conversione lunghezza

---
+++ esercizio_05/conversioni/lunghezza.py
@@ -0,0 +1,14 @@
+def metri_in_chilometri(m):
+    return m / 1000
+
+
+def chilometri_in_metri(km):
+    return km * 1000
+
+
+def metri_in_miglia(m):
+    return m / 1609.344
+
+
+def centimetri_in_pollici(cm):
+    return cm / 2.54
