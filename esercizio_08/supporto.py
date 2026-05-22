e869a1b73f00e9f2bf20429e2b891416b8151b47
davidedellabartola <davide.dellabartola2005@gmail.com>
2026-05-22 11:28:13 +0200

    aggiunto modulo supporto per funzioni aggiuntive

---
+++ esercizio_08/supporto.py
@@ -0,0 +1,17 @@
+def filtra_per_anno(veicoli, anno_minimo):
+    """Restituisce i veicoli prodotti dall'anno indicato in poi."""
+    return [v for v in veicoli if v.anno >= anno_minimo]
+
+
+def prendi_anno(veicolo):
+    return veicolo.anno
+
+def ordina_per_anno(veicoli):
+    return sorted(veicoli, key=prendi_anno)
+
+
+def stampa_lista(veicoli):
+    i = 1
+    for v in veicoli:
+        print(i, v)
+        i = i + 1
