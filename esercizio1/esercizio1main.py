8575d16161bb9d15da4330e9505e03547c22c94a
davidedellabartola <davide.dellabartola2005@gmail.com>
2026-05-22 09:25:23 +0200

    correzione

--- esercizio1/esercizio1main.py
+++ esercizio1/esercizio1main.py
@@ -1,11 +1,11 @@
-import numeri
+import esercizio1modulo
 
 a = int(input("inserisci numero a: "))
 b = int(input("inserisci numero b: "))
 
-risultato_somma = numeri.somma(a, b)
-risultato_prodotto = numeri.prodotto(a, b)
-controllo_pari = numeri.è_pari(a)
+risultato_somma = esercizio1modulo.somma(a, b)
+risultato_prodotto = esercizio1modulo.prodotto(a, b)
+controllo_pari = esercizio1modulo.è_pari(a)
 
 print(risultato_somma)
 print(risultato_prodotto)
