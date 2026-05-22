f042e966ecef8970bca55316315e6d0d6ce671ec
davidedellabartola <davide.dellabartola2005@gmail.com>
2026-05-22 09:51:06 +0200

    creato modulo logica per operazioni

---
+++ esercizio3/logica.py
@@ -0,0 +1,10 @@
+def area_rettangolo(base, altezza):
+    return base * altezza
+
+
+def perimetro_rettangolo(base, altezza):
+    return 2 * (base + altezza)
+
+
+def celsius_in_fahrenheit(celsius):
+    return celsius * 9 / 5 + 32
