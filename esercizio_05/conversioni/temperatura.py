be3e7852072395d89317e7ab236c943a80d623cc
davidedellabartola <davide.dellabartola2005@gmail.com>
2026-05-22 11:04:44 +0200

    aggiunto modulo conversione temperatura

---
+++ esercizio_05/conversioni/temperatura.py
@@ -0,0 +1,14 @@
+def celsius_in_fahrenheit(c):
+    return c * 9 / 5 + 32
+
+
+def fahrenheit_in_celsius(f):
+    return (f - 32) * 5 / 9
+
+
+def celsius_in_kelvin(c):
+    return c + 273.15
+
+
+def kelvin_in_celsius(k):
+    return k - 273.15
