c263de8fd6f6cfce30f837822d808ee12bb3f0c8
davidedellabartola <davide.dellabartola2005@gmail.com>
2026-05-22 11:03:53 +0200

    aggiunto modulo conversione peso

---
+++ esercizio_05/conversioni/peso.py
@@ -0,0 +1,14 @@
+def kg_in_grammi(kg):
+    return kg * 1000
+
+
+def grammi_in_kg(g):
+    return g / 1000
+
+
+def kg_in_libbre(kg):
+    return kg * 2.20462
+
+
+def libbre_in_kg(lb):
+    return lb / 2.20462
